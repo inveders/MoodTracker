d4c2b32c50aac0328df523b24121bbcc68b6ea16
inveders <alexandra.gnimadi@gmail.com>
2018-07-16 00:55:45 +0200

    Configure gesture detector #2.

--- app/src/main/java/com/mood/gnimadi/alexandra/moodtracker/Controller/MainActivity.java
+++ app/src/main/java/com/mood/gnimadi/alexandra/moodtracker/Controller/MainActivity.java
@@ -1,79 +1,100 @@
 package com.mood.gnimadi.alexandra.moodtracker.Controller;
 
-import android.content.Context;
-import android.gesture.GestureOverlayView;
-import android.support.v4.view.GestureDetectorCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ImageSwitcher;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.mood.gnimadi.alexandra.moodtracker.R;
 
-import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements android.view.GestureDetector.OnGestureListener {
 
-    Button mButton;
-    ImageView mImage;
-    ArrayList<ImageView> mDraw = new ArrayList<ImageView>();
-    int gesture;
+    private GestureDetector mGestureDetector;
+    private int gesture=2;
 
-    mDraw{
-        R.drawable.smiley_sad,
-        R.drawable.smiley_disappointed,
-        R.drawable.smiley_normal,
-        R.drawable.smiley_happy,
-        R.drawable.smiley_super_happy
-    }
-
+    int[] mDraw = {
+            R.drawable.smiley_sad,
+            R.drawable.smiley_disappointed,
+            R.drawable.smiley_normal,
+            R.drawable.smiley_happy,
+            R.drawable.smiley_super_happy};
+    ImageView imageSwipe = (ImageView) findViewById(R.id.draw);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);// lui il enlève
 
-        onTouchEvent();
+
+        imageSwipe.setImageResource(mDraw[gesture]);
+        mGestureDetector = new GestureDetector(this, this);
+        //setContentView(imageSwipe);
+
     }
 
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        //Toast.makeText(getApplicationContext(),"OnDown",100).show();
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+       // Toast.makeText(getApplicationContext(),"OnShow Press",100).show();
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        Toast.makeText(getApplicationContext(),"Single Tap",100).show();
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        //Toast.makeText(getApplicationContext(),"OnScroll",100).show();
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+       // Toast.makeText(getApplicationContext(),"LongPress",100).show();
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        //Toast.makeText(getApplicationContext(),"OnFlying",100).show();
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+       Image_Change();
+       return mGestureDetector.onTouchEvent(event);
+
+    }
+
+   public void Image_Change() {
+        gesture++;
+        imageSwipe.setImageResource(mDraw[gesture]);
+        System.out.println("Ok "+gesture);
+       Toast.makeText(getApplicationContext(),"Image Change",100).show();
+
+    }
+
+}
 
 
 
-
-    public class MyView extends View implements GestureOverlayView.OnGestureListener {
-
-        private GestureDetector mGestureDetector;
-        private int gesture=2;
-        public MyView(Context context) {
-            super(context);
-            mGestureDetector = new GestureDetector(this);
-        }
-
-        public boolean onTouchEvent(MotionEvent event) {
-            return mGestureDetector.onTouchEvent(event);
-            Image_Change(gesture++);
-
-
-            }
-
-
-        public boolean onDown(MotionEvent arg0) {
-            // Don't forget to return true here to get the following touch events
-            return true;
-        }
-
-        public void Image_Change(int gesture){
-
-            //mImage = (ImageView) findViewById(R.id.draw);
-
-            //SWIPE UP we add +1 to gesture
-
+//SWIPE UP we add +1 to gesture
+/*
             if (gesture<5 && gesture >=0) {
                 switch (gesture) {
                     case 0:
-                        mDraw.set(0, R.drawable.smiley_sad);
+
+                        mImage.setImageDrawable(mDraw[gesture]);
                         break;
                     case 1:
                         mDraw.set(1, R.drawable.smiley_disappointed);
@@ -96,12 +117,13 @@ public class MainActivity extends AppCompatActivity {
             else if (gesture >5) {
                 gesture = 0;
                 mDraw.set(0, R.drawable.smiley_sad);
+                getDrawable(gesture);
             }
             else (gesture <0){
                 gesture = 4;
                 mDraw.set(4, R.drawable.smiley_super_happy);
+                getResources().getIdentifier(variable,"drawable",getPackageName());
             }
         }
 
-
-    }
+*/
