b2a9d4c714ac5ef016ad354e535db28abc648391
inveders <alexandra.gnimadi@gmail.com>
2018-07-17 00:34:50 +0200

    There is no more bug during the swipe.

--- app/src/main/java/com/mood/gnimadi/alexandra/moodtracker/Controller/MainActivity.java
+++ app/src/main/java/com/mood/gnimadi/alexandra/moodtracker/Controller/MainActivity.java
@@ -1,12 +1,14 @@
 package com.mood.gnimadi.alexandra.moodtracker.Controller;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.mood.gnimadi.alexandra.moodtracker.R;
@@ -26,110 +28,121 @@ public class MainActivity extends Activity implements android.view.GestureDetect
     String[] mColor={"#ffde3c50","#ff9b9b9b","#a5468ad9","#ffb8e986","#fff9ec4f"};
 
     ImageView ImageSwipe;
-    LinearLayout Li;
+    RelativeLayout Li;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);// lui il enlève
+        setContentView(R.layout.activity_main);
 
-       ImageSwipe = (ImageView) findViewById(R.id.draw);
-       Li=(LinearLayout)findViewById(R.id.linearlayout);
+       ImageSwipe = findViewById(R.id.draw);
+       Li= findViewById(R.id.relativelayout);
        ImageSwipe.setImageResource(mDraw[gesture]);
-        mGestureDetector = new GestureDetector(this, this);
-        //setContentView(imageSwipe);
+       mGestureDetector = new GestureDetector(this, this);
+       //setContentView(imageSwipe);
 
     }
 
     @Override
     public boolean onDown(MotionEvent motionEvent) {
         //Toast.makeText(getApplicationContext(),"OnDown",100).show();
+
         return false;
     }
 
     @Override
     public void onShowPress(MotionEvent motionEvent) {
        // Toast.makeText(getApplicationContext(),"OnShow Press",100).show();
+
     }
 
     @Override
     public boolean onSingleTapUp(MotionEvent motionEvent) {
-        Toast.makeText(getApplicationContext(),"Single Tap",100).show();
+        //Toast.makeText(getApplicationContext(),"Single Tap",100).show();
+
         return true;
     }
 
     @Override
     public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
         //Toast.makeText(getApplicationContext(),"OnScroll",100).show();
+
         return false;
     }
 
     @Override
     public void onLongPress(MotionEvent motionEvent) {
        // Toast.makeText(getApplicationContext(),"LongPress",100).show();
+
     }
 
     @Override
-    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-        //Toast.makeText(getApplicationContext(),"OnFlying",100).show();
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+
+        if (e1.getX() < e2.getX()) {
+            Log.d("DEBUG", "Left to Right swipe performed");
+
+
+        }
+
+        if (e1.getX() > e2.getX()) {
+            Log.d("DEBUG", "Right to Left swipe performed");
+
+        }
+
+        if (e1.getY() < e2.getY()) {
+            Log.d("DEBUG", "Up to Down swipe performed");
+            gesture--;
+            Image_Change();
+        }
+
+        if (e1.getY() > e2.getY()) {
+            Log.d("DEBUG", "Down to Up swipe performed");
+            gesture++;
+            Image_Change();
+        }
+
         return true;
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        gesture++;
-        Image_Change();
 
-       return mGestureDetector.onTouchEvent(event);
+        return mGestureDetector.onTouchEvent(event);
 
     }
 
-   public void Image_Change() {
+    public void Image_Change() {
 
-      Toast.makeText(getApplicationContext(),"Image Change",100).show();
+        //Toast.makeText(getApplicationContext(),"Image Change",100).show();
 
-            if (gesture<5 && gesture >=0) {
-                switch (gesture) {
-                    case 0:
+        if (gesture<5 && gesture >=0) {
+            ParametersImage();
 
-                        ImageSwipe.setImageResource(mDraw[0]);
-                        Li.setBackgroundColor(Color.parseColor(mColor[0]));
-                        break;
-                    case 1:
-                        ImageSwipe.setImageResource(mDraw[1]);
-                        Li.setBackgroundColor(Color.parseColor(mColor[1]));
-                        break;
-                    case 2:
-                        ImageSwipe.setImageResource(mDraw[2]);
-                        Li.setBackgroundColor(Color.parseColor(mColor[2]));
-                        break;
-                    case 3:
-                        ImageSwipe.setImageResource(mDraw[3]);
-                        Li.setBackgroundColor(Color.parseColor(mColor[3]));
-                        break;
-                    case 4:
-                        ImageSwipe.setImageResource(mDraw[4]);
-                        Li.setBackgroundColor(Color.parseColor(mColor[4]));
-                        break;
-                    default:
-                        ImageSwipe.setImageResource(mDraw[2]);
-                        Li.setBackgroundColor(Color.parseColor(mColor[2]));
-                        break;
-
-                }
-            }
-            else if (gesture >5) {
-                gesture = 0;
-                ImageSwipe.setImageResource(mDraw[0]);
-                Li.setBackgroundColor(Color.parseColor(mColor[0]));
-                getDrawable(gesture);
-            }
-            else if (gesture <0){
-                gesture = 4;
-                ImageSwipe.setImageResource(mDraw[4]);
-                Li.setBackgroundColor(Color.parseColor(mColor[4]));
-
-            }
         }
+
+        else if (gesture >=5) {
+            gesture = 0;
+            ParametersImage();
+            //getDrawable(gesture);
+
+        }
+        else if (gesture <0){
+            gesture = 4;
+            ParametersImage();
+
+        }
+    }
+
+
+    public void ParametersImage(){
+        ImageSwipe.setImageResource(mDraw[gesture]);
+        Li.setBackgroundColor(Color.parseColor(mColor[gesture]));
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        preferences.edit().putInt("Mood", gesture);
+
+    }
 
 }
 
